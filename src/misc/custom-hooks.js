7be2948d844d8120f867220d0baece4a51ad9749
prernalele <prernalele@gmail.com>
2021-07-13 14:38:29 -0400

    created two more hooks: useLastQuery and useShow

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,18 +1,18 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useState } from 'react';
+import { apiGet } from './config';
 
-function showsReducer (prevState , action){
-
-    switch(action.type) {
-        case 'ADD': {
-            return [...prevState, action.showId]
-        }
-
-        case 'REMOVE': {
-            return prevState.filter((showId)=> showId !== action.showId)
-        }
-        default: return prevState
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
     }
 
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+    default:
+      return prevState;
+  }
 }
 function usePersistedReducer(reducer, initialState, key) {
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
@@ -24,9 +24,84 @@ function usePersistedReducer(reducer, initialState, key) {
     localStorage.setItem(key, JSON.stringify(state));
   }, [state, key]);
 
-  return [state, dispatch]
+  return [state, dispatch];
 }
 
 export function useShows(key = 'shows') {
-    return usePersistedReducer (showsReducer, [], key)
+  return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : '';
+  });
+
+  const setPersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  };
+  return [input, setPersistedInput];
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return {
+        isLoading: false,
+        show: action.show,
+        error: null,
+      };
+    }
+    case 'FETCH_FAILED': {
+      return {
+        isLoading: false,
+        show: null,
+        error: action.error,
+      };
+    }
+    default:
+      return prevState;
+  }
+};
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(
+    reducer,
+    {
+      show: null,
+      isLoading: true,
+      error: null,
+    }
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(result => {
+        setTimeout(() => {
+          if (isMounted) {
+            dispatch({
+              type: 'FETCH_SUCCESS',
+              show: result,
+            });
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({
+            type: 'FETCH_FAILED',
+            error: err.message,
+          });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [showId]);
+
+  return state
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,79 +1,24 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useReducer, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { apiGet } from '../misc/config';
 import ShowMainData from '../components/show/ShowMainData';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import Cast from '../components/show/Cast';
 import { ShowPageWrapper, InfoBlock } from './Show.styled';
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return {
-        isLoading: false,
-        show: action.show,
-        error: null,
-      };
-    }
-    case 'FETCH_FAILED': {
-      return {
-        isLoading: false,
-        show: null,
-        error: action.error,
-      };
-    }
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
+import { useShow } from '../misc/custom-hooks';
 
 const Show = () => {
   const { id } = useParams();
 
-  const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState);
+  const { show, isLoading, error } = useShow(id);
 
-  useEffect(() => {
-    let isMounted = true;
-
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(result => {
-        setTimeout(() => {
-          if (isMounted) {
-            dispatch({
-              type: 'FETCH_SUCCESS',
-              show: result,
-            });
-          }
-        }, 2000);
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({
-            type: 'FETCH_FAILED',
-            error: err.message,
-          });
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
-
-    if(isLoading) {
-        return <div> Data is being loaded </div>
-    }
-    if(error) {
-        return <div>Error Occured: {show.error}</div>
-    }
+  if (isLoading) {
+    return <div> Data is being loaded </div>;
+  }
+  if (error) {
+    return <div>Error Occured: {show.error}</div>;
+  }
 
   return (
     <ShowPageWrapper>
@@ -95,12 +40,12 @@ const Show = () => {
 
       <InfoBlock>
         <h2>Seasons</h2>
-        <Seasons seasons={show._embedded.seasons}/>
+        <Seasons seasons={show._embedded.seasons} />
       </InfoBlock>
 
       <InfoBlock>
         <h2>Cast</h2>
-        <Cast cast={show._embedded.cast}/>
+        <Cast cast={show._embedded.cast} />
       </InfoBlock>
     </ShowPageWrapper>
   );
