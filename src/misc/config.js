ff216da9d57fa0e97b70bd2339c8b65a55825768
prernalele <prernalele@gmail.com>
2021-07-15 15:14:29 -0400

    changes to deploy it on github pages

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,8 +1,9 @@
-const API_BASE_URL = "http://api.tvmaze.com"
+const API_BASE_URL = 'http://api.tvmaze.com';
 
 export async function apiGet(queryString) {
-    const response = await fetch (`${API_BASE_URL}${queryString}`).then( r => r.json())
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
 
-    return response 
+  return response;
 }
-
